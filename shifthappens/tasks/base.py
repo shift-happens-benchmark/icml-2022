43e3c15d1283a6186415625cb1f882a07c5c9b9e
zimmerrol <rzrolandzimmermann@gmail.com>
2022-03-26 10:29:53 +0100

    Follow code guidelines

--- shifthappens/tasks/base.py
+++ shifthappens/tasks/base.py
@@ -1,3 +1,5 @@
+"""Base definition of a class in the shift-happens benchmark."""
+
 from abc import ABC
 from abc import abstractmethod
 from typing import Dict
@@ -10,8 +12,8 @@ class Task(ABC):
     """Task base class."""
 
     def evaluate(self, model: sh_models.Model) -> Optional[Dict[str, float]]:
-        """"Validates that the model is compatible with the task and then evaluates the model's performance
-        using the _evaluate function of this class."""
+        """"Validates that the model is compatible with the task and then evaluates the model's
+        performance using the _evaluate function of this class."""
         if issubclass(type(self), ConfidenceTaskMixin) and not issubclass(
             type(model), model.ConfidenceModelMixin
         ):
@@ -30,8 +32,9 @@ class Task(ABC):
         return self._evaluate(model)
 
     @abstractmethod
-    def _evaluate(model: sh_models.Model) -> Dict[str, float]:
-        """Implement this function to evaluate the task and return a dictionary with the calculated metrics."""
+    def _evaluate(self, model: sh_models.Model) -> Dict[str, float]:
+        """Implement this function to evaluate the task and return a dictionary with the
+        calculated metrics."""
         pass
 
 
