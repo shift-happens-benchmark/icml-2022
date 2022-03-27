0b3aef58502d2de40762632ff5c3c9aca809e1e7
zimmerrol <rzrolandzimmermann@gmail.com>
2022-03-27 16:12:56 +0200

    Refactor code

---
+++ examples/imagenet_c.py
@@ -0,0 +1,16 @@
+from shifthappens.benchmark import register_task
+from shifthappens.tasks.metrics import Metric
+from shifthappens.tasks.base import ConfidenceTaskMixin, Task
+from shifthappens.models import Model
+
+TODO
+
+class TaskCollection():
+
+    def _evaluate():
+        # run all subtasks
+        results = ...
+        # do sth
+        return Result(
+            ...
+        )
