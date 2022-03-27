e1153c921833fe82de59363a3d4fb4c5d96baa86
zimmerrol <rzrolandzimmermann@gmail.com>
2022-03-27 11:13:50 +0200

    Improved benchmark.py

--- shifthappens/benchmark.py
+++ shifthappens/benchmark.py
@@ -1,7 +1,39 @@
+from dataclasses import dataclass
+from tasks.base import Task
+from typing import Tuple
+
+__all__ = ["registered_tasks", "run", "register_task"]
 
 
-def _collect_tasks():
-    return []
+@dataclass
+class TaskRegistration:
+    task_cls: Task
+    standalone_task: bool = True
+
+
+__registered_tasks: TaskRegistration = []
+
+
+@property
+def registered_tasks() -> Tuple[TaskRegistration]:
+    """All tasks currently registered as part of the benchmark."""
+    return tuple(__registered_tasks)
+
+
+def register_task(cls, *, standalone_task: bool = True):
+    """Register as task as part of the benchmark.
+    
+    Args:
+    standalone_task (bool): Is this task meaningful as a stand-alone task or 
+        will this only be relevant as a part of a collection of tasks?
+    """
+
+    assert issubclass(cls, Task)
+    if cls in [t.task_cls for t in registered_tasks]:
+        return
+    __registered_tasks.append(TaskRegistration(cls, standalone_task=standalone_task))
+    return cls
+
 
 def _is_compatible(tasks, model):
     # TODO
@@ -26,13 +58,21 @@ class ScoreCard():
 
 
 def run(model):
-    tasks = _collect_tasks()
+    """
+    Runs all tasks of the benchmarks for this supplied model.
+    
+    Args:
+    model (Model): 
+
+    Returns:
+
+    """
+
     results = ScoreCardEntry()
 
-    for task_cls in tasks:
+    for task_registration in registered_tasks:
+        task_cls = task_registration.task_cls
         if not _is_compatible(task, model):
             continue
         for task in task_cls.iterate_instances():
             results.add(task.run())
-
-        
