0c5af63e09be8e2bd79fb254b99329b33b99902e
Sagar Vaze <sagarvaze@Sagars-MacBook-Pro.local>
2022-07-11 22:05:09 -0400

    Add SSB out dataloader

--- shifthappens/tasks/ssb/semantic_shift_benchmark.py
+++ shifthappens/tasks/ssb/semantic_shift_benchmark.py
@@ -14,6 +14,7 @@ import os
 import numpy as np
 import torchvision.datasets as tv_datasets
 import torchvision.transforms as tv_transforms
+import torch
 
 import shifthappens.data.base as sh_data
 import shifthappens.data.torch as sh_data_torch
@@ -28,15 +29,15 @@ from shifthappens.tasks.mixins import OODScoreTaskMixin
 from shifthappens.tasks.task_result import TaskResult
 from shifthappens.tasks.utils import auroc_ood
 from shifthappens.tasks.utils import fpr_at_tpr
-
+from shifthappens.tasks.ssb.imagenet_ssb import get_imagenet_ssb_dataset
 
 @sh_benchmark.register_task(
-    name="RaccOOD", relative_data_folder="raccoons_ood", standalone=True
+    name="SSB", relative_data_folder="ssb", standalone=True
 )
 @dataclasses.dataclass
-class RaccOOD(Task, OODScoreTaskMixin):
+class SSB(Task, OODScoreTaskMixin):
     resource = (
-        "racoons",
+        "imagenet_21k_p",
         "raccoons.tar.gz",
         "https://nc.mlcloud.uni-tuebingen.de/index.php/s/JrSQeRgXfw28crC/download/raccoons.tar.gz",
         None,
@@ -45,21 +46,26 @@ class RaccOOD(Task, OODScoreTaskMixin):
     max_batch_size: int = 256
 
     def setup(self):
-        folder_name, file_name, url, md5 = self.resource
+        imagenet_21k_root, ssb_split_path, url, md5 = self.resource
         dataset_folder = os.path.join(self.data_root, folder_name)
         if not os.path.exists(dataset_folder):
             sh_utils.download_and_extract_archive(url, dataset_folder, md5, file_name)
 
-        test_transform = tv_transforms.Compose(
-            [
-                tv_transforms.ToTensor(),
-                tv_transforms.Lambda(lambda x: x.permute(1, 2, 0)),
-            ]
-        )
+        mean = (0.485, 0.456, 0.406)
+        std = (0.229, 0.224, 0.225)
 
-        dataset_out = tv_datasets.ImageFolder(
-            root=dataset_folder, transform=test_transform
-        )
+        test_transform = tv_transforms.Compose([
+            tv_transforms.Resize(256),
+            tv_transforms.CenterCrop(224),
+            tv_transforms.ToTensor(),
+            tv_transforms.Normalize(
+                mean=torch.tensor(mean),
+                std=torch.tensor(std))
+        ])
+
+        dataset_out = get_imagenet_ssb_dataset(imagenet21k_root=imagenet_21k_root, osr_split_path=ssb_split_path,
+                                               test_transform=test_transform)
+
         self.images_only_dataset_out = sh_data_torch.IndexedTorchDataset(
             sh_data_torch.ImagesOnlyTorchDataset(dataset_out)
         )
@@ -79,7 +85,7 @@ class RaccOOD(Task, OODScoreTaskMixin):
         ):
             assert (
                 predictions_out.ood_scores is not None
-            ), "OOD scores for RaccOOD task is None"
+            ), "OOD scores for SSB task is None"
             ood_scores_out_list.append(predictions_out.ood_scores)
         ood_scores_out = np.hstack(ood_scores_out_list)
         accuracy = np.equal(
