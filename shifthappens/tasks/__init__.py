670760eff76bd25c88d90d2e25c3ed2933b5bcb0
George Pachitariu <george.pachitariu@gmail.com>
2022-11-17 22:04:52 +0100

    Add ImageNet-D (#53)
    * Copy from bethgelab/robustness with minimal changes
    
    https://github.com/bethgelab/robustness/tree/7115520aa0829ccdb7d9e63081e6830e6e20e270/examples/imagenet_d
    
    Co-authored-by: Alexander Panfilov <39771221+kotekjedi@users.noreply.github.com>

--- shifthappens/tasks/__init__.py
+++ shifthappens/tasks/__init__.py
@@ -1,6 +1,7 @@
 """Utility methods and classes for the benchmark's tasks and the individual tasks."""
 
 from shifthappens.tasks import imagenet_c  # noqa: F401
+from shifthappens.tasks import imagenet_d  # noqa: F401
 from shifthappens.tasks import imagenet_r  # noqa: F401
 from shifthappens.tasks import objectnet  # noqa: F401
 from shifthappens.tasks import raccoons_ood  # noqa: F401
