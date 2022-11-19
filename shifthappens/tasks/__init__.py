71cdad060b7ff5efe308d7899a776bc9d9915c9a
Tiago Salvador <38894497+tiagosalvador@users.noreply.github.com>
2022-11-19 05:19:47 -0500

    Add imagenet_cartoon and imagenet_drawing (#80)
    Co-authored-by: Alexander Panfilov <39771221+kotekjedi@users.noreply.github.com>
    Co-authored-by: Roland Zimmermann <5895436+zimmerrol@users.noreply.github.com>

--- shifthappens/tasks/__init__.py
+++ shifthappens/tasks/__init__.py
@@ -1,7 +1,9 @@
 """Utility methods and classes for the benchmark's tasks and the individual tasks."""
 
 from shifthappens.tasks import imagenet_c  # noqa: F401
+from shifthappens.tasks import imagenet_cartoon  # noqa: F401
 from shifthappens.tasks import imagenet_d  # noqa: F401
+from shifthappens.tasks import imagenet_drawing  # noqa: F401
 from shifthappens.tasks import imagenet_m  # noqa: F401
 from shifthappens.tasks import imagenet_metashift  # noqa: F401
 from shifthappens.tasks import imagenet_patch  # noqa F401
